f010f30cd46510483175fb9029c7d27bdf4f055d
Lourine <lourine.millicent@gmail.com>
2020-07-25 17:01:06 +0300

    Add a JQUERY event to display text on portofilio on mouse hover

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,69 @@ $(document).ready(function(){
     $(this).children(".hide").toggle();
     $(this).children(".show").toggle();
   });
-})
+
+  // protofolio section
+  $(".image-frame").mouseover(function(){
+    $(".image-caption").show();
+  }).mouseout(function(){
+    $(".image-caption").hide();
+  });
+
+  $(".image-frame1").mouseover(function(){
+    $(".image-caption1").show();
+  }).mouseout(function(){
+    $(".image-caption1").hide();
+  });
+  $(".image-frame2").mouseover(function(){
+    $(".image-caption2").show();
+  }).mouseout(function(){
+    $(".image-caption2").hide();
+  });
+  $(".image-frame3").mouseover(function(){
+    $(".image-caption3").show();
+  }).mouseout(function(){
+    $(".image-caption3").hide();
+  });
+  $(".image-frame4").mouseover(function(){
+    $(".image-caption4").show();
+  }).mouseout(function(){
+    $(".image-caption4").hide();
+  });
+  $(".image-frame5").mouseover(function(){
+    $(".image-caption5").show();
+  }).mouseout(function(){
+    $(".image-caption5").hide();
+  });
+  $(".image-frame6").mouseover(function(){
+    $(".image-caption6").show();
+  }).mouseout(function(){
+    $(".image-caption6").hide();
+  });
+  $(".image-frame7").mouseover(function(){
+    $(".image-caption7").show();
+  }).mouseout(function(){
+    $(".image-caption7").hide();
+  });
+ 
+
+  //contact us form
+  
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
